2d08dea59aa8a9fa5d99e64579930a707a115a42
Juan Gamino <gamino_juan03@hotmail.com>
2020-04-21 12:46:07 -0500

    Initial Commit

---
+++ Proyectojson/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Proyectojson
+//
+//  Created by Juan on 21/04/20.
+//  Copyright © 2020 usuario. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
